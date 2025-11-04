715948a8c3c456e8ead65670ed2a091c66a043c2
Kiryl Valkovich <visortelle@gmail.com>
2025-11-04 18:17:38 +0400

    Update puls.rb

--- Formula/puls.rb
+++ Formula/puls.rb
@@ -1,11 +1,11 @@
 class Puls < Formula
   desc "The simplest way to run a local multi-cluster multi-broker Apache Pulsar instance"
-  homepage "https://github.com/tealtools/puls"
-  url "https://github.com/tealtools/puls/archive/ec38d02a45548d7615f5d2fdaf6cf7a50f244c1e.tar.gz"
+  homepage "https://github.com/visortelle/puls"
+  url "https://github.com/visortelle/puls/archive/ec38d02a45548d7615f5d2fdaf6cf7a50f244c1e.tar.gz"
   sha256 "c5dcfdfb19fa315f39092d411e98ebcfcb6801fb3beb6d183f5f85317bbfa78f"
   version "0.1.0"
   license "Apache-2.0"
-  head "https://github.com/tealtools/puls.git", branch: "main"
+  head "https://github.com/visortelle/puls.git", branch: "main"
 
   livecheck do
     url :stable
